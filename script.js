20d319ac4ed52c0203bda6e8591f59cb6c31925d
Joseph Teka <179788482+Josy216@users.noreply.github.com>
2025-02-21 12:00:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ const minute = document.getElementById("minute");
 const display = document.getElementById("demo");
 
 const togle = document.querySelector(".display");
-const togle1 = document.querySelector(".error");
 
 const btn = document.querySelector("button");
 
 btn.addEventListener("click", function minuteConvertor() {
+if(minute.value <0){
+minute.value="";
+}
 togle.style.display = "block";
 
 let converted = minute.value * 60
