5da86dea24d2abc6cb05f586ff7dd81f322f30c2
Joseph Teka <179788482+Josy216@users.noreply.github.com>
2025-02-21 11:52:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,12 +10,14 @@ const btn = document.querySelector("button");
 btn.addEventListener("click", function minuteConvertor() {
 if(minute.value < 0){
 togle1.style.display = "block";
+display.innerHTML ="minute can't be negative";
 }
-
+else{
 togle.style.display = "block";
 
 let converted = minute.value * 60
 
 display.innerHTML = minute.value + " minute is: " + converted + " seconds";
+}
 
 });
